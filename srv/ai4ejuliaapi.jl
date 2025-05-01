b8c360c0132dea40c07e7c9e86af8640122cb5b4
Mingtao Li <mingtao@xjtu.edu.cn>
2025-05-01 15:44:28 +0800

    修改端口号

--- srv/ai4ejuliaapi.jl
+++ srv/ai4ejuliaapi.jl
@@ -1,5 +1,5 @@
 using Ai4EJuliaApi
 Ai4EJuliaApi.InitRouter()
-port = parse(Int, get(ENV, "PORT", "9801"))
+port = parse(Int, get(ENV, "PORT", "9001"))
 # Ai4EJuliaApi.Oxygen.serve(host="0.0.0.0", port=19801, show_banner=false, reuseaddr=true)
 Ai4EJuliaApi.Oxygen.serve(host="0.0.0.0", port=port, show_banner=false)
